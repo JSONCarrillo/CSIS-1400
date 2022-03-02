21115dcf55b1113075da112e30c29d1b1836188a
jayce <jaycelinux@gmail.com>
2022-03-01 20:21:19 -0700

    add comments explaing code

--- people-weight-lab/PeopleWeights.java
+++ people-weight-lab/PeopleWeights.java
@@ -2,28 +2,35 @@ import java.util.Scanner;
 
 public class PeopleWeights {
    public static void main(String[] args) {
+      //initialize variables
       Scanner scnr = new Scanner(System.in);
       double[] weights = new double[5];
       double totalWeight, avgWeight, maxWeight;
       totalWeight = avgWeight = maxWeight = 0;
       
+      //for loop that asks for user input given the pre-determined length of the array
       for(int i = 0; i < weights.length;i++){
          System.out.println("Enter weight " + (i + 1) + ":");
+	 //saves the value the user input into the index of the array
          weights[i] = scnr.nextDouble();
+	 //adds to total weight
          totalWeight += weights[i];
+	 //checks if current weight is larger than the current max weight, if it is greater, it will be assigned the new max weight
          if(weights[i] > maxWeight){
             maxWeight = weights[i];
          }
       }
+      //generates average weight
       avgWeight = totalWeight / 5;
       
+      //prints all weights entered by user
       System.out.print("You entered: ");
       for(int i = 0; i < weights.length;i++){
          System.out.print(weights[i] + " ");
       }
       System.out.println("\n");
       
-      
+      //prints total, average, and max weights respectively
       System.out.println("Total weight: " + totalWeight);
       System.out.println("Average weight: " + avgWeight);
       System.out.println("Max weight: " + maxWeight);
